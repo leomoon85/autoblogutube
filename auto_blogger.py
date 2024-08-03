8f0c93338efefe7632fc60a2a98b332e725019e6
leomoon85 <leo.moon85@gmail.com>
2024-08-03 18:22:55 +0530

    Update auto_blogger.py

--- auto_blogger.py
+++ auto_blogger.py
@@ -1,5 +1,4 @@
 import os
-import requests
 import json
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -8,14 +7,12 @@ from googleapiclient.discovery import build
 
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+PLAYLIST_ID = 'PLefHOXeb70TKiVBztN7cOQt6PuOkOxeCx'  # Playlist ID
 
 def authenticate_youtube_api():
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -23,71 +20,70 @@ def authenticate_youtube_api():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'client_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
 
-# Get top videos until we find 10 with captions
-def get_videos_with_captions(youtube, max_results=50):
-    videos_with_captions = []
+def get_videos_with_descriptions(youtube, playlist_id, max_results=50):
+    videos_with_descriptions = []
     next_page_token = None
 
-    while len(videos_with_captions) < 1000000:
-        request = youtube.videos().list(
+    while len(videos_with_descriptions) < 10:
+        request = youtube.playlistItems().list(
             part='snippet',
-            chart='mostPopular',
+            playlistId=playlist_id,
             maxResults=max_results,
-            pageToken=next_page_token,
-            regionCode='US'
+            pageToken=next_page_token
         )
         response = request.execute()
         
-        for video in response['items']:
-            video_id = video['id']
-            video_title = video['snippet']['title']
-            captions = get_captions(video_id)
-            if captions:
-                videos_with_captions.append({
+        for item in response['items']:
+            video_id = item['snippet']['resourceId']['videoId']
+            video_title = item['snippet']['title']
+            video_description = get_description(youtube, video_id)
+            if video_description:
+                videos_with_descriptions.append({
                     'id': video_id,
                     'title': video_title,
-                    'captions': captions
+                    'description': video_description
                 })
-                if len(videos_with_captions) == 10:
+                if len(videos_with_descriptions) == 10:
                     break
         
         next_page_token = response.get('nextPageToken')
         if not next_page_token:
             break
 
-    return videos_with_captions
+    return videos_with_descriptions
 
-# Get captions for a video
-def get_captions(video_id):
-    url = f'http://video.google.com/timedtext?lang=en&v={video_id}'
-    response = requests.get(url)
-    return response.text if response.status_code == 200 else None
+def get_description(youtube, video_id):
+    request = youtube.videos().list(
+        part='snippet',
+        id=video_id
+    )
+    response = request.execute()
+    if response['items']:
+        return response['items'][0]['snippet']['description']
+    return None
 
-# Main function
 def main():
     creds = authenticate_youtube_api()
     youtube = build('youtube', 'v3', credentials=creds)
     
-    videos = get_videos_with_captions(youtube)
+    videos = get_videos_with_descriptions(youtube, PLAYLIST_ID)
     
-    if not os.path.exists('captions'):
-        os.makedirs('captions')
+    if not os.path.exists('descriptions'):
+        os.makedirs('descriptions')
     
     for video in videos:
         video_id = video['id']
         video_title = video['title']
-        captions = video['captions']
-        # Sanitize the file name
+        video_description = video['description']
         sanitized_title = ''.join(c for c in video_title if c.isalnum() or c in (' ', '_')).rstrip()
         
-        with open(f'captions/{sanitized_title}.txt', 'w', encoding='utf-8') as f:
-            f.write(captions)
-        print(f'Captions saved for video: {video_title}')
+        with open(f'descriptions/{sanitized_title}.txt', 'w', encoding='utf-8') as f:
+            f.write(video_description)
+        print(f'Description saved for video: {video_title}')
 
 if __name__ == "__main__":
     main()
